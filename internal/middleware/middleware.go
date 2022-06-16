2c13f409e1336515b5911cf50220a04268fc158c
Anastasiya Bardanos <bardanos@Anastasiyas-MacBook-Pro.local>
2022-06-16 21:21:32 +0200

    add

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"context"
+	"github.com/fortuna91/ya_praktikum_final/internal/auth"
+	"github.com/fortuna91/ya_praktikum_final/internal/handlers"
+	"net/http"
+)
+
+func Authorization(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/api/user/register" || r.URL.Path == "/api/user/login" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		token, err := auth.GetTokenFromHeader(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		login, errParse := auth.ParseToken(token)
+		if errParse != nil {
+			http.Error(w, errParse.Error(), http.StatusUnauthorized)
+			return
+		}
+		if user := handlers.DB.GetUser(context.Background(), login); user == nil {
+			http.Error(w, "unknown user", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+		return
+	})
+}
---
+++ internal/utils/utils.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+)
+
+func GetBody(readerBody io.ReadCloser) *[]byte {
+	defer readerBody.Close()
+
+	respBody, err := ioutil.ReadAll(readerBody)
+	if err != nil {
+		log.Printf("Couldn't read body %v\n", err)
+		return nil
+	}
+	return &respBody
+}
